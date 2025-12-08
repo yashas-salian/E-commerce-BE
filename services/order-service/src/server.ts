bac25eec4af12aedbaef7784887d3afbf3dc194f
Your Name <youremail@example.com>
2025-12-08 12:33:52 +0530

    log package publish

--- services/order-service/src/server.ts
+++ services/order-service/src/server.ts
@@ -1,9 +1,9 @@
 import express, {type Express} from "express"
 import orderRouter from './routes/orders.route.js'
-
+import { autoLogger } from "@shared/log"
 
 const app: Express = express()
 
 app.use('/orders', orderRouter)
 
-export default app
+export default app  
