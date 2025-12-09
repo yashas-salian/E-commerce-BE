cc7519d5469dc187846948497d87e2b0392bd889
Your Name <youremail@example.com>
2025-12-09 14:56:05 +0530

    fix db package

--- services/repositories/db/models/order.model.ts
+++ services/repositories/db/models/order.model.ts
@@ -3,12 +3,12 @@ export const createOrderSchema = zod.object({
     userId: zod.string(),
     items: zod.array(
         zod.object({
-            userId: zod.string(),
-            orderId: zod.string(),
+            // userId: zod.string(),
+            // orderId: zod.string(),
             productId: zod.string(),
             quantity: zod.number().int(),
             unitPrice: zod.number()
         })
-    )
+    ).min(1, "Order must have at least one item")
 })
     
